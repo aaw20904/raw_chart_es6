9df8ef156af5b7807448b298a0a8ac4dc3ef595c
Andrii Androsovych <55437123+aaw20904@users.noreply.github.com>
2021-03-28 22:29:50 +0300

    Add files via upload

---
+++ canvas.js
@@ -0,0 +1,175 @@
+let paintSubClass = (function(){
+    let m = new WeakMap();
+    let privateMembers = {
+      /**brief: returns currrent window limits
+       * return: {w,h}
+       */
+      getWindowLimits: function () {
+       
+        return {w:window.innerWidth, h:window.innerHeight};
+      },
+      setCanvasLimits: function (windowLim={w:100,h:100}, lim, canvasId) {
+        /*assign limit*/
+        lim.w = windowLim.w;
+        /*in according to Fibonaci nuimbers*/
+        lim.h = lim.w * 0.66;
+        /*assignto the canvas*/
+        let canv = document.getElementById(canvasId);
+        if (canv) {
+           canv.width = lim.w;
+           canv.height = lim.h;   
+           console.log('Canvas has setted '+ canv.width + ':' + canv.height);
+        }
+      },
+
+      paintIemOnCanvas (maximumArg, parToShow, color="blue", startX, width, limits) {
+        /*get a 2d context*/
+        let cnv = document.getElementById("canvas001");
+        let ctx = cnv.getContext("2d");
+        ctx.fillStyle = color; 
+        let stepY = limits.h * 0.1;
+        let stepX =  limits.w * 0.1;
+        let rectY, rectH;
+        let maxLen = stepY * 8;
+        rectH = (maxLen / maximumArg) * parToShow;
+         /*start point*/
+         rectY = (maxLen - rectH) + (stepY * 1);
+         ctx.fillRect(startX,rectY,width,rectH);
+      },
+
+      /***private***variables ***/
+       limits: {},//limits of current canvas
+       count: 0, //max count of items on the diagram
+       maxArg: 0,
+    }
+
+    class paintSubClass {
+      constructor () {
+        /*bind to this to make a private members*/
+          m.set(this,privateMembers);
+          privateMembers.setCanvasLimits(privateMembers.getWindowLimits(), privateMembers.limits,"canvas001");
+      }
+
+      paintScale (color="black",y=100) {
+        let privMemb = m.get(this);
+        privMemb.maxArg = y;
+        let numberStep = y / 4;
+        /*get a 2d context*/
+        let cnv = document.getElementById("canvas001");
+        let ctx = cnv.getContext("2d");
+        ctx.font =  `${Math.round((privMemb.limits.h * 0.05)+2).toString()}px Arial, serif`;
+        ctx.strokeStyle = color;
+        ctx.strokeWidth = 2;
+        ctx.fillStyle = color; 
+        /*calculating a rate = y to real width*/
+        /*dividing Y size on 10 pieces */
+        let stepY = privMemb.limits.h * 0.1;
+        let stepX =  privMemb.limits.w * 0.1;
+        /*@ ZERO in according to a Canvas API - UPPER LEFT point on a canvasNode*/ 
+        ctx.beginPath();
+        ctx.moveTo(stepX, (stepY * 9));
+        /*a horizontal line*/
+        ctx.lineTo((stepX * 8), (stepY * 9));
+        ctx.stroke();
+        /*paint an horizontal arrow*/
+        ctx.beginPath();
+        ctx.moveTo((stepX * 8), (stepY * 9));
+        ctx.lineTo((stepX * 7.8), (stepY * 8.9));
+        ctx.lineTo((stepX * 7.8), (stepY * 9.1));
+     
+        ctx.closePath();
+        ctx.stroke();
+        ctx.fill();
+        /*move to zero*/
+        ctx.beginPath();
+        ctx.moveTo (stepX, (stepY * 9));
+        /*a vertical line*/
+        ctx.lineTo (stepX,stepY * 0.9); 
+        ctx.stroke ();    
+        /*paint an verticall  arrow*/
+        ctx.beginPath();
+        ctx.moveTo(stepX, stepY * 0.5 );
+        ctx.lineTo((stepX * 0.92), (stepY * 0.9));
+        ctx.lineTo((stepX * 1.08), (stepY * 0.9));
+        ctx.closePath();
+        ctx.stroke();
+        ctx.fill();
+        /*paint a mark lines */
+        let scaleStep = (stepY * 8) / 4;
+        ctx.beginPath();
+        ctx.moveTo(stepX,  stepY);//to zero
+        ctx.lineTo((stepX * 0.8),  stepY);
+        ctx.moveTo(stepX, ((scaleStep * 1) + stepY));//the first line-marker
+        ctx.lineTo((stepX * 0.9), (scaleStep * 1) + stepY);
+        ctx.moveTo(stepX, (scaleStep * 2) + stepY);//the first line-marker
+        ctx.lineTo((stepX * 0.9), (scaleStep * 2) + stepY);
+        ctx.moveTo(stepX, (scaleStep * 3) + stepY);//the thrid line-marker
+        ctx.lineTo((stepX * 0.9), (scaleStep * 3) + stepY);
+        ctx.moveTo(stepX, (scaleStep * 4) + stepY);//the forth line-marker
+        ctx.lineTo((stepX * 0.9), (scaleStep * 4) + stepY);
+        ctx.moveTo(stepX, (scaleStep * 5) + stepY);//the forth line-marker
+        ctx.lineTo((stepX * 0.9), (scaleStep * 5) + stepY);
+        
+        /*paint a numbers*/
+        
+        ctx.fillText(Math.round(numberStep * 1), (stepX * 0.2), (scaleStep * 3) + stepY, stepY);
+        ctx.fillText(Math.round(numberStep * 2), (stepX * 0.2), (scaleStep * 2) + stepY, stepY);
+        ctx.fillText(Math.round(numberStep * 3), (stepX * 0.2),   (scaleStep * 1) + stepY, stepY);
+        ctx.fillText(Math.round(numberStep * 4), (stepX * 0.2),   stepY, stepY);
+      
+        ctx.stroke();
+      }
+
+      paintDiagramData (data) {
+        let iter;
+        let lengthOfY;
+        let privMem = m.get(this);
+          /*set count*/
+        privMem.count = data.length;
+          if(data.length > 7){
+            return;
+          }
+           /*calculating a rate = y to real width*/
+        /*dividing Y size on 10 pieces */
+        let stepY = privMemb.limits.h * 0.1;
+        let stepX =  privMemb.limits.w * 0.1;
+        /*a count of pixels per one item*/
+        let itemStepX = (stepX * 8) / data.length;
+        let cnv = document.getElementById("canvas001");
+        let ctx = cnv.getContext("2d");
+        /*color set*/
+        let colorSet = new Set();
+        colorSet.add("black");
+        colorSet.add("red");
+        colorSet.add("orange");
+        colorSet.add("yellow");
+        colorSet.add("green");
+        colorSet.add("blue");
+        colorSet.add("indigo");
+        colorSet.add("violet");
+        iter = data[Symbol.iterator]();
+        let result = iter.next();
+        let offset = stepX;
+        while (!result.done) {
+        lengthOfY = ((stepY * 8) / privMem.maxArg) * result.value;
+         ctx.fillRect(offset + 2, ((stepY * 8) - lengthOfY) + stepY)
+            
+          
+          coordX += itemStep;
+        }
+      }
+
+      test () {
+        let privMem = m.get(this);
+        privMem.paintIemOnCanvas(100,100,"blue",60, 15, privMem.limits);
+      }
+
+    }
+ return paintSubClass;
+})();
+
+window.onload = ()=>{
+  let q = new paintSubClass();
+  q.paintScale("blue",100);
+  q.test();
+}
